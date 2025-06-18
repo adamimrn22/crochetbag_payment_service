8cefc2b806c9d4be730e95a63fc381a8f54f4881
Adam Imran <adamimran.dev@gmail.com>
2025-06-19 01:48:02 +0800

    push to vercel

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   res.json({ status: "OK", timestamp: new Date().toISOString() });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
